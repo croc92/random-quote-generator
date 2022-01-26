b03ae38ee1e0634a4052f7ae65cd5eebe7bd0057
Hakan <hakan-ayata@hotmail.com>
2022-01-26 17:36:16 +0100

    quote-box width fixed, API added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Wrapper from './components/Wrapper'
 
-const quotes = [
-  {
-    author: 'Steve Jobs',
-    quote: "Your time is limited, so don't waste it living someone else's life.",
-  },
-  {
-    author: 'Robert Louis Stevenson',
-    quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
-  },
-  {
-    author: 'Benjamin Franklin',
-    quote: 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
-  },
-  {
-    author: 'Plato',
-    quote: 'We can easily forgive a child who is afraid of the dark; the real tragedy of life is when men are afraid of the light.',
-  },
-  {
-    author: 'Farrah Gray',
-    quote: 'Build your own dreams, or someone else will hire you to build theirs.',
-  },
-];
+let quotes = [];
 
-const colors = ['#faebd7', '#b1fde4', '#bcf4f8', '#d0ffd0' , '#f5f5f5', '#ececbc' , '#dfb8b8', '#e6c5e9' , '#d1aaf5', '#8ed2ff', '#fffd8e']
+const colors = ['#faebd7', '#b1fde4', '#bcf4f8', '#d0ffd0', '#f5f5f5', '#ececbc', '#dfb8b8', '#e6c5e9', '#d1aaf5', '#8ed2ff', '#fffd8e']
 
 const getRandomQuote = () => {
   const randomIndex = Math.floor(Math.random() * quotes.length)
@@ -36,10 +15,9 @@ const getRandomColor = () => {
   return colors[randomIndex]
 }
 
-
 const App = () => {
-
-  const [randomQuote, setRandomQuote] = useState(getRandomQuote());
+  console.log('render')
+  const [randomQuote, setRandomQuote] = useState(null);
   const [randomColor, setRandomColor] = useState(getRandomColor());
   const [animation, setAnimation] = useState(false);
 
@@ -47,13 +25,26 @@ const App = () => {
     setRandomQuote(getRandomQuote());
     setRandomColor(getRandomColor());
     setAnimation(!animation)
-    console.log(randomColor)
   }
 
+  useEffect(() => {
+
+    fetch('https://type.fit/api/quotes')
+      .then(response => response.json())
+      .then(data => {
+        quotes = data
+        changeQuote()
+      })
+      
+  }, [])
+
+  if (randomQuote == null) {
+    return null
+  } 
 
   return <div>
     <Wrapper 
-      randomQuote={randomQuote.quote}
+      randomQuote={randomQuote.text}
       randomAuthor={randomQuote.author}
       randomColor={randomColor}
       changeQuote={changeQuote}
